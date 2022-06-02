8385327417b4e31d60d1044d876c256d3edce82d
jitp <jitp@mywr.cn>
2022-06-02 08:50:36 +0800

    update

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import App from './App'
 import { routerBeforeEach } from './utils/routerBeforeEach'
 // 路由跳转公用方法
 uni.$push = routerBeforeEach
+
+
 import { createSSRApp } from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
